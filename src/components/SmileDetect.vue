3e38d82778301833914e7510a2460de4138bcf6e
Douglas Glover <douglas.glover@outlook.com>
2021-04-28 01:24:17 -0400

    expression detection functioning on static images

--- src/components/SmileDetect.vue
+++ src/components/SmileDetect.vue
@@ -1,14 +1,30 @@
 <template>
   <div>
     <h2>Smile detect</h2>
+    <img src="/smiling-test-1.jpg" alt="stock photo" id="test-human" />
+    <img src="/test_tree.png" alt="stock photo" id="test-tree" />
   </div>
 </template>
 
 <script>
 import * as faceapi from "face-api.js";
 export default {
+  methods: {
+    async initDetection() {
+      const MODEL_URL = "/models";
+      await faceapi.loadFaceLandmarkModel(MODEL_URL);
+      await faceapi.loadFaceExpressionModel(MODEL_URL);
+      await faceapi.loadTinyFaceDetectorModel(MODEL_URL);
+
+      const detectionsWithExpressions = await faceapi
+        .detectAllFaces(["test-human"], new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withFaceExpressions();
+      console.log(detectionsWithExpressions[0].expressions);
+    },
+  },
   mounted() {
-    console.log(faceapi);
+    this.initDetection();
   },
 };
 </script>
